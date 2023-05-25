4f4299b5f9d21b446d1e0acd0569183970b0304c
RedRing <info@redring.jp>
2023-05-26 02:58:49 +0900

    Vector2dメソッドの実装

--- src/Bulbasaur/Vector2d.cpp
+++ src/Bulbasaur/Vector2d.cpp
@@ -1,5 +1,21 @@
 #include "Vector2d.h"
 
-Vector2d::Vector2d() :X(0.0), Y(0.0)
+Vector2d::Vector2d() : X(0.0), Y(0.0)
 {
-};
+}
+
+Vector2d::Vector2d(double x, double y) : X(x), Y(y)
+{
+}
+
+Vector2d Vector2d::Zero()
+{
+	return Vector2d(0.0, 0.0);
+}
+
+Vector2d Vector2d::XAxis()
+{
+	return Vector2d(1.0, 0.0);
+}
+
+
