1155ea59418225cb7f089752b36ef121303fc2d3
RedRing <info@redring.jp>
2023-05-26 02:59:50 +0900

    Vector2dヘッダーコミット漏れ

--- src/Bulbasaur/Vector2D.h
+++ src/Bulbasaur/Vector2D.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Vector2d.h"
+
+struct Vector2d {
+	double X;
+	double Y;
+
+public:
+	Vector2d();
+	Vector2d(double x, double y);
+
+	static Vector2d Zero();
+	static Vector2d XAxis();
+};
+
+
+
